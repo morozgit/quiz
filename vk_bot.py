aad35d2c809faef4c8c5367f38adaac44cd71208
morozgit <morozec76@gmail.com>
2023-12-26 16:43:13 +0300

    add vk bot

---
+++ vk_bot.py
@@ -0,0 +1,67 @@
+import os
+import random
+
+import redis
+import vk_api as vk
+from dotenv import find_dotenv, load_dotenv
+from vk_api.keyboard import VkKeyboard, VkKeyboardColor
+from vk_api.longpoll import VkEventType, VkLongPoll
+from vk_api.utils import get_random_id
+
+from quiz import quiz_answers, quiz_questions
+
+QUESTION_ID = 0
+
+
+def get_keyboard(event, vk_api):
+    keyboard = VkKeyboard(one_time=True)
+    keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
+    keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
+    keyboard.add_line()
+    keyboard.add_button('Мой счет', color=VkKeyboardColor.SECONDARY)
+    vk_api.messages.send(
+                user_id=event.user_id,
+                random_id=get_random_id(),
+                keyboard=keyboard.get_keyboard(),
+                message=event.message
+            )
+
+def handle_new_question_request(event, vk_api):
+    if event.text == 'Новый вопрос':
+        r.set(event.user_id, quiz_questions[f'Вопрос {QUESTION_ID}'])
+        question = r.get(event.user_id)
+        vk_api.messages.send(
+            user_id=event.user_id,
+            message=question,
+            random_id=random.randint(1,1000)
+        )
+        # return handle_solution_attempt(event, vk_api)
+
+def handle_solution_attempt(event, vk_api):
+    global QUESTION_ID
+    if event.text.split('.')[0] in quiz_answers[f'Ответ {QUESTION_ID}']:
+        vk_api.messages.send(
+                user_id=event.user_id,
+                message='Правильно!',
+                random_id=random.randint(1,1000)
+            )
+    else:
+        vk_api.messages.send(
+                    user_id=event.user_id,
+                    message='Неправильно… Попробуешь ещё раз?',
+                    random_id=random.randint(1,1000)
+                )
+    QUESTION_ID += 1
+
+if __name__ == '__main__':
+    pool = redis.ConnectionPool(host='localhost', port=6379, db=0, decode_responses=True)
+    r = redis.Redis(connection_pool=pool)
+    load_dotenv(find_dotenv())
+    vk_token = os.environ.get("VK_TOKEN")
+    vk_session = vk.VkApi(token=vk_token)
+    vk_api = vk_session.get_api()
+    longpoll = VkLongPoll(vk_session)
+
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me:
+            get_keyboard(event, vk_api)
